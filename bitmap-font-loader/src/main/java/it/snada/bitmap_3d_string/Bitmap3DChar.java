ed6f2cd4168c3682de995c3414b7c0aff09a6fcf
snada <stefano.nada@gmail.com>
2017-08-06 01:19:08 +0200

    Docs and tests for 3D Chars

--- bitmap-font-loader/src/main/java/it/snada/bitmap_3d_string/Bitmap3DChar.java
+++ bitmap-font-loader/src/main/java/it/snada/bitmap_3d_string/Bitmap3DChar.java
@@ -9,7 +9,8 @@ import java.nio.FloatBuffer;
 import it.snada.bitmap_font_loader.BitmapChar;
 
 /**
- * Helper class to handle 3d bitmap chars
+ * Sample class to represent a single 3D char of a 3D string.
+ * Contains transformation for the single char as well as uv coordinates and buffers of the bitmap texture
  */
 public class Bitmap3DChar extends Bitmap3DObject{
     Bitmap3DString string3D;
@@ -18,6 +19,12 @@ public class Bitmap3DChar extends Bitmap3DObject{
 
     private FloatBuffer uvBuffer;
 
+    /**
+     * Creates a 3D char
+     * @param string3D a Bitmap3DString instance
+     * @param bitmapChar the char of the string
+     * @param cursorPosition passed by the Bitmap3DString containing this 3D char, is the cursor position
+     */
     public Bitmap3DChar(Bitmap3DString string3D, BitmapChar bitmapChar, float cursorPosition) {
         super(
             cursorPosition + bitmapChar.getXOffset(),
@@ -51,17 +58,25 @@ public class Bitmap3DChar extends Bitmap3DObject{
         return this.uvBuffer;
     }
 
+    /**
+     * Gets the matrix that will be used to translate this 3D char
+     * @return float[] object
+     */
     public float[] getTranslationMatrix() {
         Matrix.setIdentityM(this.translationMatrix, 0);
         Matrix.translateM(
             this.translationMatrix, 0,
-            this.string3D.getPositionX() + (this.getPositionX()),
-            this.string3D.getPositionY() + (this.getPositionY()),
-            this.string3D.getPositionZ() + (this.getPositionZ())
+            this.string3D.getPositionX() + this.getPositionX(),
+            this.string3D.getPositionY() + this.getPositionY(),
+            this.string3D.getPositionZ() + this.getPositionZ()
         );
         return this.translationMatrix;
     }
 
+    /**
+     * Gets the matrix that will be used to rotate this 3D char
+     * @return float[] object
+     */
     public float[] getRotationMatrix() {
         Matrix.setIdentityM(this.rotationMatrix, 0);
         //Painful Android bug. Could be done with:
@@ -74,6 +89,10 @@ public class Bitmap3DChar extends Bitmap3DObject{
         return this.rotationMatrix;
     }
 
+    /**
+     * Gets the matrix that will be used to scale this 3D char
+     * @return float[] object
+     */
     public float[] getScaleMatrix() {
         Matrix.setIdentityM(scaleMatrix, 0);
 
@@ -105,38 +124,78 @@ public class Bitmap3DChar extends Bitmap3DObject{
         return modelMatrix;
     }
 
+    /**
+     * U value for top left corner of the quad
+     * @return float value
+     */
     public float getTopLeftU() {
         return reverseLerp(0, this.string3D.getBitmapFont().getScaleW(), bitmapChar.getX());
     }
 
+    /**
+     * V value for top left corner of the quad
+     * @return float value
+     */
     public float getTopLeftV() {
         return reverseLerp(0, this.string3D.getBitmapFont().getScaleH(), bitmapChar.getY());
     }
 
+    /**
+     * U value for bottom left corner of the quad
+     * @return float value
+     */
     public float getBottomLeftU() {
         return reverseLerp(0, this.string3D.getBitmapFont().getScaleW(), bitmapChar.getX());
     }
 
+    /**
+     * V value for bottom left corner of the quad
+     * @return float value
+     */
     public float getBottomLeftV() {
         return reverseLerp(0, this.string3D.getBitmapFont().getScaleH(), bitmapChar.getY() + bitmapChar.getHeight());
     }
 
+    /**
+     * U value for top right corner of the quad
+     * @return float value
+     */
     public float getTopRightU() {
         return reverseLerp(0, this.string3D.getBitmapFont().getScaleW(), bitmapChar.getX() + bitmapChar.getWidth());
     }
 
+    /**
+     * V value for top right corner of the quad
+     * @return float value
+     */
     public float getTopRightV() {
         return reverseLerp(0, this.string3D.getBitmapFont().getScaleH(), bitmapChar.getY());
     }
 
+    /**
+     * U value for bottom right corner of the quad
+     * @return float value
+     */
     public float getBottomRightU() {
         return reverseLerp(0, this.string3D.getBitmapFont().getScaleW(), bitmapChar.getX() + bitmapChar.getWidth());
     }
 
+    /**
+     * V value for bottom right corner of the quad
+     * @return float value
+     */
     public float getBottomRightV() {
         return reverseLerp(0, this.string3D.getBitmapFont().getScaleH(), bitmapChar.getY() + bitmapChar.getHeight());
     }
 
+    /**
+     * Linear interpolation inverse, from two values and a middle one, getting t value.
+     * Transforms a texture pixel dimension in a [0, 1] range coordinate
+     * @param a first value
+     * @param b second value
+     * @param value value between a and b
+     * @return the linear interpolation value between 0 and 1 that would return value
+     */
     private static float reverseLerp(float a, float b, float value) {
         return (value - a) / (b - a);
     }
